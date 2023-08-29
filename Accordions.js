64ab1c81b3eb28c3727608662651399f11bdfba8
manishkumarpatidar <manishpatidar78@gmail.com>
2023-08-29 17:14:15 +0530

    Activites page

---
+++ Accordions.js
@@ -0,0 +1,128 @@
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import ArrowForwardIosSharpIcon from '@mui/icons-material/ArrowForwardIosSharp';
+import MuiAccordion from '@mui/material/Accordion';
+import MuiAccordionSummary from '@mui/material/AccordionSummary';
+import MuiAccordionDetails from '@mui/material/AccordionDetails';
+import Typography from '@mui/material/Typography';
+ 
+
+const Accordion = styled((props) => (
+  <MuiAccordion disableGutters elevation={0} square {...props} />
+))(({ theme }) => ({
+  border: `1px solid ${theme.palette.divider}`,
+  '&:not(:last-child)': {
+    borderBottom: 0,
+  },
+  '&:before': {
+    display: 'none',
+  },
+}));
+
+const AccordionSummary = styled((props) => (
+  <MuiAccordionSummary
+    expandIcon={<ArrowForwardIosSharpIcon sx={{ fontSize: '0.9rem' }} />}
+    {...props}
+  />
+))(({ theme }) => ({
+  backgroundColor:
+    theme.palette.mode === 'dark'
+      ? 'rgba(255, 255, 255, .05)'
+      : 'rgba(0, 0, 0, .03)',
+  flexDirection: 'row-reverse',
+  '& .MuiAccordionSummary-expandIconWrapper.Mui-expanded': {
+    transform: 'rotate(90deg)',
+  },
+  '& .MuiAccordionSummary-content': {
+    marginLeft: theme.spacing(1),
+  },
+}));
+
+const AccordionDetails = styled(MuiAccordionDetails)(({ theme }) => ({
+  padding: theme.spacing(2),
+  borderTop: '1px solid rgba(0, 0, 0, .125)',
+}));
+
+export default function Accordions() {
+  const [expanded, setExpanded] = React.useState('panel1');
+
+  const handleChange = (panel) => (event, newExpanded) => {
+    setExpanded(newExpanded ? panel : false);
+  };
+
+  return (
+    <>
+    <div className='accordion1'>
+      <Accordion expanded={expanded === 'panel1'} onChange={handleChange('panel1')}>
+        <AccordionSummary aria-controls="panel1d-content" id="panel1d-header">
+        <div style={{display:"flex",justifyContent:"space-between"}}>
+          <div >
+          <Typography>Date</Typography>
+            </div>
+            <div><button> Clear</button></div>
+          </div>  
+          <div>
+            <button>Today</button>
+            <button>Tomorrow</button>
+            <button>This Weekend</button>
+          </div>
+        </AccordionSummary>
+      </Accordion>
+    </div>
+    <div className='accordion1'>
+      <Accordion expanded={expanded === 'panel1'} onChange={handleChange('panel1')}>
+        <AccordionSummary aria-controls="panel1d-content" id="panel1d-header">
+        <div style={{display:"flex",justifyContent:"space-between"}}>
+          <div >
+          <Typography>Categories</Typography>
+            </div>
+            <div><button> Clear</button></div>
+          </div>  
+          <div>
+            <button>Adventure</button>
+            <button>Unique Tours</button>
+            <button>Foods and Drinks</button>
+            <button>Gaming</button>
+          </div>
+        </AccordionSummary>
+      </Accordion>
+    </div>
+    <div className='accordion1'>
+      <Accordion expanded={expanded === 'panel1'} onChange={handleChange('panel1')}>
+        <AccordionSummary aria-controls="panel1d-content" id="panel1d-header">
+        <div style={{display:"flex",justifyContent:"space-between"}}>
+          <div >
+          <Typography>More Filters</Typography>
+            </div>
+            <div><button> Clear</button></div>
+          </div>  
+          <div>
+            <button>Outdoor Events</button>
+            <button>Kids Allowed</button>
+            <button>Fast Filling</button>
+          </div>
+        </AccordionSummary>
+      </Accordion>
+    </div>  
+    <div className='accordion1'>
+      <Accordion expanded={expanded === 'panel1'} onChange={handleChange('panel1')}>
+        <AccordionSummary aria-controls="panel1d-content" id="panel1d-header">
+        <div style={{display:"flex",justifyContent:"space-between"}}>
+          <div >
+          <Typography>Price</Typography>
+            </div>
+            <div><button> Clear</button></div>
+          </div>  
+          <div>
+            <button>Free </button>
+            <button>0-500</button>
+            <button>501-2000</button>
+            <button>Above 2000</button>
+          </div>
+        </AccordionSummary>
+      </Accordion>
+    </div>
+    <div><button>Browse by Venues </button></div>
+    </>
+  );
+}
